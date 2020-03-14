14d484def8fa3d3f00063629510b821fcda79d1d
Kate Pronina <kateadm@narod.ru>
2020-03-14 13:53:40 +0300

    Выполненное задание

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,16 @@
-export default class Character {
-  constructor(name) {
-    this.name = name;
-    this.level = 1;
-    this.health = 100;
+export default class ErrorRepository {
+  constructor() {
+    this.errors = new Map();
+    this.errors.set(400, 'Bad Request');
+    this.errors.set(401, 'Unauthorized');
+    this.errors.set(403, 'Forbidden');
+    this.errors.set(404, 'Not Found');
+    this.errors.set(408, 'Request Timeout');
+    this.errors.set(500, 'Internal Server Error');
+  }
+
+  translate(code) {
+    if (!this.errors.has(code)) throw new Error('Unknown error');
+    return this.errors.get(code);
   }
 }
